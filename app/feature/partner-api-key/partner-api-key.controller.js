2936cfb552c4ebf8085d84014125c207ccb6730b
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-03-23 19:35:17 +0700

    get list api key & create api key

---
+++ app/feature/partner-api-key/partner-api-key.controller.js
@@ -0,0 +1,40 @@
+const logger = require("app/lib/logger");
+const config = require("app/config");
+const StakingAPI = require("app/lib/staking-api/partner-api-key")
+
+module.exports = {
+    getAll: async(req, res, next) => {
+        try {
+            let items = await StakingAPI.getAllApiKey(req.params.id);
+            if (items.data) {
+                return res.ok(items.data);
+            }
+            else {
+                return res.ok([]);
+            }
+        }
+        catch (err) {
+            logger.error("get list grandchild fail:", err);
+            next(err);
+        }
+    },
+    create: async(req, res, next) => {
+		try {
+            body = {
+                partner_id: req.params.id,
+                name: req.body.name
+            }
+            let items = await StakingAPI.createApiKey(body)
+            if (items.data) {
+				return res.ok(items.data);
+			}
+			else {
+				return res.ok([]);
+			}
+		}
+		catch (err) {
+			logger.error("create grandchild fail:", err);
+			next(err);
+		}
+	},
+}
---
+++ app/lib/staking-api/partner-api-key.js
@@ -0,0 +1,62 @@
+const config = require("app/config");
+const axios = require("axios");
+const logger = require("app/lib/logger")
+const redisResource = require("app/resource/redis");
+const redis = require("app/lib/redis");
+const cache = redis.client();
+
+module.exports = {
+    getAllApiKey:  async (partner_id) => {
+        try {
+          let accessToken = await _getToken();
+          let result = await axios.get(`${config.stakingApi.url}/partners/${partner_id}/keys`, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`
+            }
+          });
+    
+          return result.data;
+        }
+        catch (err) {
+          logger.error("get list API key fail:", err);
+          return err.response.data;
+        }
+      },
+      createApiKey: async(body) => {
+        try {
+          let accessToken = await _getToken();
+          let result = await axios.post(`${config.stakingApi.url}/partners/${body.partner_id}/keys`, 
+          body,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`
+            }
+          });
+    
+          return result.data;
+        }
+        catch (err) {
+          logger.error("create new API key fail:", err);
+          return err.response.data;
+        }
+      }
+}
+async function _getToken() {
+    let token = await cache.getAsync(redisResource.stakingApi.token);
+    if (token) {
+      return token;
+    }
+    let result = await axios.post(
+      `${config.stakingApi.url}/accounts/authentication`,
+      {
+        api_key: config.stakingApi.key,
+        secret_key: config.stakingApi.secret,
+        grant_type: "client_credentials"
+      }
+    );
+  
+    await cache.setAsync(redisResource.stakingApi.token, result.data.data.access_token, "EX", parseInt(result.data.data.expires_in) - 10);
+    return result.data.data.access_token;
+  }
