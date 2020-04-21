8b424f5de6a8527ed070e77d43ca87aadea66343
Unknown <huyht@blockchainlabs.asia>
2020-04-21 11:30:31 +0700

    fix get role
    fix get role

--- app/feature/user/user.controller.js
+++ app/feature/user/user.controller.js
@@ -447,7 +447,13 @@ async function _getRoleControl(roles) {
     });
 
     if (role) {
-      roleControl.push(role.id)
+      let roles = await Role.findAll({
+        where: {
+          level: role.level,
+          deleted_flg: false
+        }
+      });
+      roleControl = roleControl.concat(roles.map(x => x.id));
     }
   }
 
