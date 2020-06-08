657308046c0bc0e15eb3579da538ee699d6b5802
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-06-08 20:51:12 +0700

    install queryString and member feature

--- app/feature/member/member.controller.js
+++ app/feature/member/member.controller.js
@@ -1,39 +1,9 @@
 const logger = require('app/lib/logger');
-const Member = require("app/model/wallet").members;
-const MemberStatus = require("app/model/wallet/value-object/member-status");
-const userMapper = require("app/feature/response-schema/user.response-schema");
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-const config = require("app/config");
-const database = require('app/lib/database').db().wallet;
-const Role = require("app/model/wallet").roles;
-const UserRole = require("app/model/wallet").user_roles;
 const MembershipApi = require("app/lib/membership-api");
 
 module.exports = {
   search: async (req, res, next) => {
     try {
-      // let limit = req.query.limit ? parseInt(req.query.limit) : 10;
-      // let offset = req.query.offset ? parseInt(req.query.offset) : 0;
-      // let where = {
-      //   deleted_flg: false
-      // };
-
-      // let criteria = [];
-      // if (req.query.member_sts) where.member_sts = req.query.member_sts;
-      // if (req.query.email) criteria.push({ email: { [Op.iLike]: `%${req.query.email}%` } });
-      // if (req.query.address) criteria.push({ address: { [Op.iLike]: `%${req.query.address}%` } });
-      // if (req.query.fullname) criteria.push({ fullname: { [Op.iLike]: `%${req.query.fullname}%` } });
-      // if (req.query.phone) criteria.push({ phone: req.query.phone });
-      // if (criteria.length > 0) where[Op.or] = criteria;
-
-      // const { count: total, rows: items } = await Member.findAndCountAll({ limit, offset, where: where, order: [['created_at', 'DESC']] });      
-      // return res.ok({
-      //   items: userMapper(items) && items.length>0?userMapper(items):[],
-      //   offset: offset,
-      //   limit: limit,
-      //   total: total
-      // });
       // let items = await MembershipApi.searchMember(req.query);
       // if (!items.code) {
       //   return res.ok(items.data);
@@ -42,8 +12,9 @@ module.exports = {
       //   return res.status(parseInt(items.code)).send(items.data);
       // }
 
+      // region memo data
       let data = {
-        items : [
+        items: [
           {
             id: '1',
             name: 'User1',
@@ -65,16 +36,65 @@ module.exports = {
             create_at: '2020-04-29 15:15:51.676+09'
           }
         ],
-        total : 2,
-        limit : 10,
-        offset : 0,
+        total: 2,
+        limit: 10,
+        offset: 0,
       }
-      console.log(data.items);
+      //end region
       return res.ok(data);
     }
     catch (err) {
       logger.error('search member fail:', err);
       next(err);
     }
-  }
+  },
+
+  getMemberDetail: async (req, res, next) => {
+    try {
+      let { params } = req;
+      // let items = await MembershipApi.getMemberDetail(memberId);
+      // if (!items.code) {
+      //   return res.ok(items.data);
+      // }
+      // else {
+      //   return res.status(parseInt(items.code)).send(items.data);
+      // }
+
+      // region memo data
+      let data = {
+        id: '2',
+        name: 'User2',
+        membership_type: 'Paid',
+        kyc_level: 'KYC 1',
+        kyc_status: 'Approved',
+        referral_code: '1FE2MK',
+        referrer: '0FA2MN',
+        create_at: '2020-04-29 15:15:51.676+09'
+      }
+      //end region
+      return res.ok(data);
+    }
+    catch (err) {
+      logger.error('get member detail fail:', err);
+      next(err);
+    }
+  },
+  updateMember: async (req, res, next) => {
+    try {
+      let { body, params } = req;
+      // let items = await MembershipApi.updateMember(memberId,body);
+      // if (!items.code) {
+      //   return res.ok(items.data);
+      // }
+      // else {
+      //   return res.status(parseInt(items.code)).send(items.data);
+      // }
+
+      return res.ok(true);
+    }
+    catch (err) {
+      logger.error('get member detail fail:', err);
+      next(err);
+    }
+  },
 }
--- app/lib/membership-api/index.js
+++ app/lib/membership-api/index.js
@@ -4,6 +4,7 @@ const logger = require("app/lib/logger")
 const redisResource = require("app/resource/redis");
 const redis = require("app/lib/redis");
 const cache = redis.client();
+const queryString = require('query-string');
 // const { getToken } = require("./token")
 
 module.exports = {
@@ -12,13 +13,17 @@ module.exports = {
             let limit = query.limit ? parseInt(query.limit) : 10;
             let offset = query.offset ? parseInt(query.offset) : 0;
             
-            let queryString = `limit=${limit}&offset=${offset}`;
-            if (query.name) queryString = queryString +`&name=${query.name}`;
-            if (query.email) queryString = queryString +`&email=${query.email}`;
-            if (query.membershipTypeId) queryString = queryString +`&membershipTypeId=${query.membershipTypeId}`;
-            if (query.referralCode) queryString = queryString +`&referralCode=${query.referralCode}`;
-            if (query.referrer) queryString = queryString +`&referrer=${query.referrer}`;
-            console.log(queryString);
+            const data = {
+                limit: limit,
+                offset: offset
+            }
+            
+            if (query.name) data.name = query.name;
+            if (query.email) data.email = query.email;
+            if (query.membershipTypeId) data.membershipTypeId = query.membershipTypeId;
+            if (query.referralCode) data.referralCode = query.referralCode;
+            if (query.referrer) data.referrer = query.referrer;
+            queryString.stringify(data);
             let accessToken = await getToken();
             let result = await axios.get(`${config.membershipApi.url}/members/?${queryString}`, {
                 headers: {
@@ -45,7 +50,28 @@ module.exports = {
             return result.data;
         }
         catch (err) {
-            logger.error("get list member fail:", err);
+            logger.error("get member detail fail:", err);
+            return { code: err.response.status, data: err.response.data };
+        }
+    },
+    updateMember: async (memberId,body) =>{
+        try {
+            let accessToken = await getToken();
+            let result = await axios.put(`${config.membershipApi.url}/members/${memberId}`, 
+            {
+                membershipTypeId: body.membershipTypeId,
+                referrer: body.referrer
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            return result.data;
+        }
+        catch (err) {
+            logger.error("update member fail:", err);
             return { code: err.response.status, data: err.response.data };
         }
     },
