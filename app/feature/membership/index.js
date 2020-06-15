a77e87ee65b4aa1870a21b4ee3ef62a54cc7a2ea
nqt900 <nqt900@gmail.com>
2020-06-15 13:26:46 +0700

    add query order

--- app/feature/membership/index.js
+++ app/feature/membership/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require("./bank-account/bank-account.route"));
+router.use(require("./membership-order/membership-order.route"))
 
 module.exports = router;
--- app/feature/membership/membership-order/membership-order.route.js
+++ app/feature/membership/membership-order/membership-order.route.js
@@ -30,7 +30,7 @@ module.exports = router;
 
 /**
  * @swagger
- * /web/members:
+ * /web/membership/orders:
  *   get:
  *     summary: search member
  *     tags:
