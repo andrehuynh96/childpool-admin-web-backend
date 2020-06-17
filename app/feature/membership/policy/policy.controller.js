928035512574877cd6f1701c85b67e9fd23228e2
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-06-17 11:58:14 +0700

    api update policy

--- app/feature/membership/policy/policy.controller.js
+++ app/feature/membership/policy/policy.controller.js
@@ -1,6 +1,5 @@
 const logger = require('app/lib/logger');
 const affiliateApi = require('app/lib/affiliate-api');
-
 module.exports = {
     getAllPolicy: async (req, res, next) => {
         try {
@@ -12,6 +11,20 @@ module.exports = {
         } 
         catch (error) {
             logger.info('get policy list fail',error);
+            next(error);
+        }
+    },
+    updatePolicy: async (req,res, next) => {
+        try {
+            const { body, params } = req;
+            const policyId = params.policyId;
+            const result = await affiliateApi.updatePolicy( policyId, body);
+            if (result.httpCode !== 200) {
+                return res.status(result.httpCode).send(result.data);
+            }
+            return res.ok(result.data);
+        } catch (error) {
+            logger.info('update policy fail',error);
             next(error);
         }
     },
--- app/feature/membership/policy/policy.route.js
+++ app/feature/membership/policy/policy.route.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const authenticate = require('app/middleware/authenticate.middleware');
 const controller = require('./policy.controller');
-const validator = require("app/middleware/validator.middleware");
-// const requestSchema = require('./member.request-schema');
 const authority = require('app/middleware/authority.middleware');
 const PermissionKey = require('app/model/wallet/value-object/permission-key');
 
@@ -14,7 +12,15 @@ router.get(
     authority(PermissionKey.MEMBERSHIP_VIEW_POLICY_LIST),
     controller.getAllPolicy
 );
+
+router.put(
+    '/policies/:policyId',
+    authenticate,
+    authority(PermissionKey.MEMBERSHIP_UPDATE_POLICY),
+    controller.updatePolicy
+);
 module.exports = router;
+
 
 /** *******************************************************************/
 /**
@@ -55,6 +61,89 @@ module.exports = router;
                         "offset": 0,
                         "limit": 9007199254740991,
                         "total": 1
+                    }
+                }
+ *       400:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/400'
+ *       401:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       404:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/404'
+ *       500:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+
+ /**
+ * @swagger
+ * /web/membership/policies/{policyId}:
+ *   put:
+ *     summary: update policy
+ *     tags:
+ *       - Policy
+ *     description: update user profile
+ *     parameters:
+ *       - name: policyId
+ *         in: path
+ *         type: string
+ *         required: true
+ *       - name: data
+ *         in: body
+ *         required: true
+ *         description: submit data JSON to update.
+ *         schema:
+ *            type: object
+ *            required:
+ *            - name
+ *            - description
+ *            - proportion_share
+ *            - max_levels
+ *            - rates
+ *            example:
+ *                  {
+                        "name": "Membership system - Affiliate Policy",
+                        "description": "",
+                        "proportion_share": 10,
+                        "max_levels": 5,
+                        "rates": [
+                            "50",
+                            "30",
+                            "10",
+                            "7",
+                            "3"
+                        ]
+                    }
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         examples:
+ *           application/json:
+ *             {
+                    "data": {
+                        "id": 4,
+                        "name": "Membership system - Affiliate Policy",
+                        "description": "",
+                        "type": "AFFILIATE",
+                        "proportion_share": "10",
+                        "max_levels": 5,
+                        "rates": [
+                            "50",
+                            "30",
+                            "10",
+                            "7",
+                            "3"
+                        ],
+                        "created_at": "2020-05-08T06:32:06.283Z",
+                        "updated_at": "2020-06-17T04:55:15.468Z"
                     }
                 }
  *       400:
