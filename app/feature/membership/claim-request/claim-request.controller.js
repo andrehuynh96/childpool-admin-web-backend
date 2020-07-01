d58d92bdbadfeb379f1ba82f9cf6df64e450126d
Unknown <huyht@blockchainlabs.asia>
2020-07-01 14:54:11 +0700

    add system-type to claim-request
    add system-type to claim-request

--- app/feature/membership/claim-request/claim-request.controller.js
+++ app/feature/membership/claim-request/claim-request.controller.js
@@ -12,284 +12,288 @@ const Op = Sequelize.Op;
 const PaymentType = require("app/model/wallet/value-object/claim-request-payment-type");
 const Platform = require("app/model/wallet/value-object/platform");
 const blockchainHelpper = require('app/lib/blockchain-helpper');
+const SystemType = require("app/model/wallet/value-object/system-type");
 
 module.exports = {
-    search: async (req, res, next) => {
-        try {
-            const { query } = req;
-            const limit = query.limit ? parseInt(req.query.limit) : 10;
-            const offset = query.offset ? parseInt(req.query.offset) : 0;
-            const where = {};
-            let fromDate, toDate;
-            if (query.from_date || query.to_date) {
-                where.created_at = {};
-            }
-            if (query.from_date) {
-                fromDate = moment(query.from_date).toDate();
-                where.created_at[Op.gte] = fromDate;
-            }
-            if (query.to_date) {
-                toDate = moment(query.to_date).add(1, 'minute').toDate();
-                where.created_at[Op.lt] = toDate;
-            }
-            if (fromDate && toDate && fromDate >= toDate) {
-                return res.badRequest(res.__("TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE"), "TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE", { field: ['from_date', 'to_date'] });
-            }
-            if (query.payment) {
-                where.type = query.payment;
-            }
-            if (query.status) {
-                where.status = query.status;
-            }
-            if (query.crypto_platform) {
-                where.currency_symbol = query.crypto_platform;
-            }
-            const memberCond = {
-                deleted_flg: false
-            };
-            if (query.email) {
-                memberCond.email = { [Op.iLike]: `%${query.email}%` };
-            }
-            const { count: total, rows: items } = await ClaimRequest.findAndCountAll({
-                limit,
-                offset,
-                include: [
-                    {
-                        attributes: ['email'],
-                        as: "Member",
-                        model: Member,
-                        where: memberCond,
-                        required: true
-                    }
-                ],
-                where: where,
-                order: [['created_at', 'DESC']]
-            });
+  search: async (req, res, next) => {
+    try {
+      const { query } = req;
+      const limit = query.limit ? parseInt(req.query.limit) : 10;
+      const offset = query.offset ? parseInt(req.query.offset) : 0;
+      const where = { system_type: SystemType.MEMBERSHIP };
+      let fromDate, toDate;
+      if (query.from_date || query.to_date) {
+        where.created_at = {};
+      }
+      if (query.from_date) {
+        fromDate = moment(query.from_date).toDate();
+        where.created_at[Op.gte] = fromDate;
+      }
+      if (query.to_date) {
+        toDate = moment(query.to_date).add(1, 'minute').toDate();
+        where.created_at[Op.lt] = toDate;
+      }
+      if (fromDate && toDate && fromDate >= toDate) {
+        return res.badRequest(res.__("TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE"), "TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE", { field: ['from_date', 'to_date'] });
+      }
+      if (query.payment) {
+        where.type = query.payment;
+      }
+      if (query.status) {
+        where.status = query.status;
+      }
+      if (query.crypto_platform) {
+        where.currency_symbol = query.crypto_platform;
+      }
+      const memberCond = {
+        deleted_flg: false
+      };
+      if (query.email) {
+        memberCond.email = { [Op.iLike]: `%${query.email}%` };
+      }
+      const { count: total, rows: items } = await ClaimRequest.findAndCountAll({
+        limit,
+        offset,
+        include: [
+          {
+            attributes: ['email'],
+            as: "Member",
+            model: Member,
+            where: memberCond,
+            required: true
+          }
+        ],
+        where: where,
+        order: [['created_at', 'DESC']]
+      });
 
-            return res.ok({
-                items: mapper(items) && items.length > 0 ? mapper(items) : [],
-                offset: offset,
-                limit: limit,
-                total: total
-            });
-        }
-        catch (error) {
-            logger.info('get claim request list fail', error);
-            next(error);
-        }
-    },
-    getDetail: async (req, res, next) => {
-        try {
-            const claimRequest = await ClaimRequest.findOne({
-                include: [
-                    {
-                        attributes: ['email'],
-                        as: "Member",
-                        model: Member,
-                        where: {
-                            deleted_flg: false
-                        },
-                        required: true
-                    }
-                ],
-                where: {
-                    id: req.params.claimRequestId
-                }
-            });
-
-            if (!claimRequest) {
-                return res.badRequest(res.__("CLAIM_REQUEST_NOT_FOUND"), "CLAIM_REQUEST_NOT_FOUND", { field: ['claimRequestId'] });
-            }
-            claimRequest.explorer_link = blockchainHelpper.getUrlTxid(claimRequest.txid, claimRequest.currency_symbol);
-            return res.ok(claimRequest);
-        }
-        catch (error) {
-            logger.info('get claim request detail fail', error);
-            next(error);
-        }
-    },
-    updateTxid: async (req, res, next) => {
-        try {
-            const { body, params } = req;
-            const claimRequest = await ClaimRequest.findOne({
-                where: {
-                    id: params.claimRequestId
-                }
-            });
-            if (!claimRequest) {
-                return res.badRequest(res.__("CLAIM_REQUEST_NOT_FOUND"), "CLAIM_REQUEST_NOT_FOUND", { field: ['claimRequestId'] });
-            }
-           await ClaimRequest.update(
-                { txid: body.txid },
-                {
-                    where: {
-                        id: claimRequest.id
-                    }
-                }
-            );
-            return res.ok(true);
-        }
-        catch (error) {
-            logger.info('update claim request tx_id fail', error);
-            next(error);
-        }
-    },
-    changeStatus: async (req, res, next) => {
-        try {
-            const { body, params } = req;
-            const claimRequest = await ClaimRequest.findOne({
-                where: {
-                    id: params.claimRequestId
-                }
-            });
-
-            if (!claimRequest) {
-                return res.badRequest(res.__("CLAIM_REQUEST_NOT_FOUND"), "CLAIM_REQUEST_NOT_FOUND", { field: ['claimRequestId'] });
-            }
-
-            if (claimRequest.status !== ClaimRequestStatus.Pending) {
-                return res.badRequest(res.__("CAN_NOT_APPROVE_REJECT_CLAIM_REQUEST"), "CAN_NOT_APPROVE_REJECT_CLAIM_REQUEST", { field: ['claimRequestId'] });
-            }
-
-            const transaction = await database.transaction();
-            try {
-                await ClaimRequest.update(
-                    { status: body.status },
-                    {
-                        where: {
-                            id: claimRequest.id
-                        },
-                        transaction: transaction,
-                        returning: true
-                    });
-
-                const result = await membershipApi.updateClaimRequest(claimRequest.affiliate_claim_reward_id, body.status);
-
-                if (result.httpCode !== 200) {
-                    await transaction.rollback();
-
-                    return res.status(result.httpCode).send(result.data);
-                }
-
-                await transaction.commit();
-                return res.ok(true);
-            }
-            catch (error) {
-                await transaction.rollback();
-                throw error;
-            }
-        }
-        catch (error) {
-            logger.info('update claim request fail', error);
-            next(error);
-        }
-    },
-    downloadCSV: async (req, res, next) => {
-        try {
-            const { query } = req;
-            const where = {};
-            let fromDate, toDate;
-            if (query.from_date || query.to_date) {
-                where.created_at = {};
-            }
-            if (query.from_date) {
-                fromDate = moment(query.from_date).toDate();
-                where.created_at[Op.gte] = fromDate;
-            }
-            if (query.to_date) {
-                toDate = moment(query.to_date).add(1, 'minute').toDate();
-                where.created_at[Op.lt] = toDate;
-            }
-            if (fromDate && toDate && fromDate >= toDate) {
-                return res.badRequest(res.__("TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE"), "TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE", { field: ['from_date', 'to_date'] });
-            }
-            if (query.payment) {
-                where.type = query.payment;
-            }
-            if (query.status) {
-                where.status = query.status
-            }
-            if (query.crypto_platform) {
-                where.currency_symbol = query.crypto_platform;
-            }
-            let memberCond = {
-                deleted_flg: false
-            };
-            if (query.email) {
-                memberCond.email = { [Op.iLike]: `%${query.email}%` };
-            }
-            const items = await ClaimRequest.findAll({
-                include: [
-                    {
-                        attributes: ['email', 'fullname'],
-                        as: "Member",
-                        model: Member,
-                        where: memberCond,
-                        required: true
-                    }
-                ],
-                where: where,
-                order: [['created_at', 'DESC']]
-            });
-
-            items.forEach(element => {
-                element.member_email = element.Member.email;
-                element.created_at = moment(element.createdAt).format('YYYY-MM-DD HH:mm');
-            });
-            const data = await stringifyAsync(items, [
-                { key: 'id', header: 'Id' },
-                { key: 'created_at', header: 'Time' },
-                { key: 'member_email', header: 'Email' },
-                { key: 'wallet_address', header: 'Wallet Address' },
-                { key: 'amount', header: 'Claim Amount' },
-                { key: 'currency_symbol', header: 'Currency' },
-                { key: 'account_number', header: 'Account Number' },
-                { key: 'bank_name', header: 'Bank Name' },
-                { key: 'branch_name', header: 'Branch Name' },
-                { key: 'account_holder', header: 'Account Holder' },
-                { key: 'status', header: 'Status' },
-                { key: 'type', header: 'Payment' }
-            ]);
-            res.setHeader('Content-disposition', 'attachment; filename=claim-request.csv');
-            res.set('Content-Type', 'text/csv');
-            res.send(data);
-        }
-        catch (error) {
-            logger.info('download csv fail', error);
-            next(error);
-        }
-    },
-    getPaymentType: async (req, res, next) => {
-        try {
-            return res.ok(PaymentType);
-        }
-        catch (error) {
-            logger.info('get payment type fail', error);
-            next(error);
-        }
-    },
-    getCryptoPlatform: async (req, res, next) => {
-        try {
-            return res.ok(Platform);
-        }
-        catch (error) {
-            logger.info('get crypto platform fail', error);
-            next(error);
-        }
+      return res.ok({
+        items: mapper(items) && items.length > 0 ? mapper(items) : [],
+        offset: offset,
+        limit: limit,
+        total: total
+      });
     }
+    catch (error) {
+      logger.info('get claim request list fail', error);
+      next(error);
+    }
+  },
+  getDetail: async (req, res, next) => {
+    try {
+      const claimRequest = await ClaimRequest.findOne({
+        include: [
+          {
+            attributes: ['email'],
+            as: "Member",
+            model: Member,
+            where: {
+              deleted_flg: false,
+              system_type: SystemType.MEMBERSHIP
+            },
+            required: true
+          }
+        ],
+        where: {
+          id: req.params.claimRequestId
+        }
+      });
+
+      if (!claimRequest) {
+        return res.badRequest(res.__("CLAIM_REQUEST_NOT_FOUND"), "CLAIM_REQUEST_NOT_FOUND", { field: ['claimRequestId'] });
+      }
+      claimRequest.explorer_link = blockchainHelpper.getUrlTxid(claimRequest.txid, claimRequest.currency_symbol);
+      return res.ok(claimRequest);
+    }
+    catch (error) {
+      logger.info('get claim request detail fail', error);
+      next(error);
+    }
+  },
+  updateTxid: async (req, res, next) => {
+    try {
+      const { body, params } = req;
+      const claimRequest = await ClaimRequest.findOne({
+        where: {
+          id: params.claimRequestId,
+          system_type: SystemType.MEMBERSHIP
+        }
+      });
+      if (!claimRequest) {
+        return res.badRequest(res.__("CLAIM_REQUEST_NOT_FOUND"), "CLAIM_REQUEST_NOT_FOUND", { field: ['claimRequestId'] });
+      }
+      await ClaimRequest.update(
+        { txid: body.txid },
+        {
+          where: {
+            id: claimRequest.id
+          }
+        }
+      );
+      return res.ok(true);
+    }
+    catch (error) {
+      logger.info('update claim request tx_id fail', error);
+      next(error);
+    }
+  },
+  changeStatus: async (req, res, next) => {
+    try {
+      const { body, params } = req;
+      const claimRequest = await ClaimRequest.findOne({
+        where: {
+          id: params.claimRequestId,
+          system_type: SystemType.MEMBERSHIP
+        }
+      });
+
+      if (!claimRequest) {
+        return res.badRequest(res.__("CLAIM_REQUEST_NOT_FOUND"), "CLAIM_REQUEST_NOT_FOUND", { field: ['claimRequestId'] });
+      }
+
+      if (claimRequest.status !== ClaimRequestStatus.Pending) {
+        return res.badRequest(res.__("CAN_NOT_APPROVE_REJECT_CLAIM_REQUEST"), "CAN_NOT_APPROVE_REJECT_CLAIM_REQUEST", { field: ['claimRequestId'] });
+      }
+
+      const transaction = await database.transaction();
+      try {
+        await ClaimRequest.update(
+          { status: body.status },
+          {
+            where: {
+              id: claimRequest.id
+            },
+            transaction: transaction,
+            returning: true
+          });
+
+        const result = await membershipApi.updateClaimRequest(claimRequest.affiliate_claim_reward_id, body.status);
+
+        if (result.httpCode !== 200) {
+          await transaction.rollback();
+
+          return res.status(result.httpCode).send(result.data);
+        }
+
+        await transaction.commit();
+        return res.ok(true);
+      }
+      catch (error) {
+        await transaction.rollback();
+        throw error;
+      }
+    }
+    catch (error) {
+      logger.info('update claim request fail', error);
+      next(error);
+    }
+  },
+  downloadCSV: async (req, res, next) => {
+    try {
+      const { query } = req;
+      const where = { system_type: SystemType.MEMBERSHIP };
+      let fromDate, toDate;
+      if (query.from_date || query.to_date) {
+        where.created_at = {};
+      }
+      if (query.from_date) {
+        fromDate = moment(query.from_date).toDate();
+        where.created_at[Op.gte] = fromDate;
+      }
+      if (query.to_date) {
+        toDate = moment(query.to_date).add(1, 'minute').toDate();
+        where.created_at[Op.lt] = toDate;
+      }
+      if (fromDate && toDate && fromDate >= toDate) {
+        return res.badRequest(res.__("TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE"), "TO_DATE_MUST_BE_GREATER_THAN_OR_EQUAL_FROM_DATE", { field: ['from_date', 'to_date'] });
+      }
+      if (query.payment) {
+        where.type = query.payment;
+      }
+      if (query.status) {
+        where.status = query.status
+      }
+      if (query.crypto_platform) {
+        where.currency_symbol = query.crypto_platform;
+      }
+      let memberCond = {
+        deleted_flg: false
+      };
+      if (query.email) {
+        memberCond.email = { [Op.iLike]: `%${query.email}%` };
+      }
+      const items = await ClaimRequest.findAll({
+        include: [
+          {
+            attributes: ['email', 'fullname'],
+            as: "Member",
+            model: Member,
+            where: memberCond,
+            required: true
+          }
+        ],
+        where: where,
+        order: [['created_at', 'DESC']]
+      });
+
+      items.forEach(element => {
+        element.member_email = element.Member.email;
+        element.created_at = moment(element.createdAt).format('YYYY-MM-DD HH:mm');
+      });
+      const data = await stringifyAsync(items, [
+        { key: 'id', header: 'Id' },
+        { key: 'created_at', header: 'Time' },
+        { key: 'member_email', header: 'Email' },
+        { key: 'wallet_address', header: 'Wallet Address' },
+        { key: 'amount', header: 'Claim Amount' },
+        { key: 'currency_symbol', header: 'Currency' },
+        { key: 'account_number', header: 'Account Number' },
+        { key: 'bank_name', header: 'Bank Name' },
+        { key: 'branch_name', header: 'Branch Name' },
+        { key: 'account_holder', header: 'Account Holder' },
+        { key: 'status', header: 'Status' },
+        { key: 'type', header: 'Payment' }
+      ]);
+      res.setHeader('Content-disposition', 'attachment; filename=claim-request.csv');
+      res.set('Content-Type', 'text/csv');
+      res.send(data);
+    }
+    catch (error) {
+      logger.info('download csv fail', error);
+      next(error);
+    }
+  },
+  getPaymentType: async (req, res, next) => {
+    try {
+      return res.ok(PaymentType);
+    }
+    catch (error) {
+      logger.info('get payment type fail', error);
+      next(error);
+    }
+  },
+  getCryptoPlatform: async (req, res, next) => {
+    try {
+      return res.ok(Platform);
+    }
+    catch (error) {
+      logger.info('get crypto platform fail', error);
+      next(error);
+    }
+  }
 };
 
 function stringifyAsync(data, columns) {
-    return new Promise(function (resolve, reject) {
-        stringify(data, {
-            header: true,
-            columns: columns
-        }, function (err, data) {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(data);
-        }
-        );
-    });
+  return new Promise(function (resolve, reject) {
+    stringify(data, {
+      header: true,
+      columns: columns
+    }, function (err, data) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    }
+    );
+  });
 }
--- app/model/wallet/claim-request.js
+++ app/model/wallet/claim-request.js
@@ -1,76 +1,82 @@
 const ClaimRequestStatus = require("./value-object/claim-request-status");
+const SystemType = require('./value-object/system-type');
 
 module.exports = (sequelize, DataTypes) => {
-    const ClaimRequest = sequelize.define("claim_requests", {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        member_id: {
-            type: DataTypes.UUID,
-            allowNull: false
-        },
-        member_account_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        type: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        status: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            defaultValue: ClaimRequestStatus.Pending
-        },
-        amount: {
-            type: DataTypes.DECIMAL,
-            allowNull: false,
-        },
-        currency_symbol: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        account_number: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        bank_name: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        branch_name: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        account_holder: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        wallet_address: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        txid: {
-            type: DataTypes.STRING(500),
-            allowNull: true,
-        },
-        affiliate_claim_reward_id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-        },
-    }, {
-        underscored: true,
-        timestamps: true,
+  const ClaimRequest = sequelize.define("claim_requests", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    member_id: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    member_account_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: ClaimRequestStatus.Pending
+    },
+    amount: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    currency_symbol: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    account_number: {
+      type: DataTypes.STRING(250),
+      allowNull: true
+    },
+    bank_name: {
+      type: DataTypes.STRING(250),
+      allowNull: true
+    },
+    branch_name: {
+      type: DataTypes.STRING(250),
+      allowNull: true
+    },
+    account_holder: {
+      type: DataTypes.STRING(250),
+      allowNull: true
+    },
+    wallet_address: {
+      type: DataTypes.STRING(250),
+      allowNull: true
+    },
+    txid: {
+      type: DataTypes.STRING(500),
+      allowNull: true,
+    },
+    affiliate_claim_reward_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    system_type: {
+      type: DataTypes.STRING(125),
+      allowNull: true,
+      defaultValue: SystemType.MEMBERSHIP
+    }
+  }, {
+      underscored: true,
+      timestamps: true,
     });
 
-    ClaimRequest.associate = (models) => {
-        ClaimRequest.belongsTo(models.members, {
-            as: 'Member',
-            foreignKey: 'member_id',
-        });
-    };
+  ClaimRequest.associate = (models) => {
+    ClaimRequest.belongsTo(models.members, {
+      as: 'Member',
+      foreignKey: 'member_id',
+    });
+  };
 
-    return ClaimRequest;
+  return ClaimRequest;
 };
