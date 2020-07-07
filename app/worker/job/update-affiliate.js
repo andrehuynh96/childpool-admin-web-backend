1264423ab420ead392fc03339c7d29789e4d93b3
nqt900 <nqt900@gmail.com>
2020-07-07 14:11:39 +0700

    temp

--- app/worker/job/update-affiliate.js
+++ app/worker/job/update-affiliate.js
@@ -1,9 +1,22 @@
+const AccountContributionAPI = require("../../lib/staking-api/account-contribution")
 module.exports = {
-    start: ()=>{
+    start: async ()=>{
         let coins = ["ATOM", "IRIS", "ONT"]
         coins.forEach(element => {
-           // call account distribution
-            
+            // call account distribution
+            let contributions = await AccountContributionAPI.get(element, 20, 0)
+            let ids = []
+            contributions.forEach(async (contribution) =>{
+                let address = contribution.address                
+                let amount = contribution.amount
+                // find user of address
+
+                // update affiliate
+
+                ids.push(contribution.id)
+            })
+            // update account contribution
+            await AccountContributionAPI.set(element, {ids: ids})
         });
     }
 }
