01387ed598663b77893cb264e45632929eb8f1a9
ducnt <ducnt@blockchainlabs.asia>
2020-07-10 14:01:22 +0700

    delete field unused

--- app/feature/membership/fiat-rate/validator/update.js
+++ app/feature/membership/fiat-rate/validator/update.js
@@ -1,8 +1,7 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-  usd_rate_by_jpy: Joi.number().greater(0).required(),
-  usd_rate_by_jpy_updated_at: Joi.string().max(50).required(),
+  usd_rate_by_jpy: Joi.number().greater(0).required()
 });
 
 module.exports = schema;
