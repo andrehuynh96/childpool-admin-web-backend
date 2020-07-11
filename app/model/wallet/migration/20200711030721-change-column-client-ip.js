7cd1d129f26a5f199842e0f0c31327104aa41cd0
nqt900 <nqt900@gmail.com>
2020-07-11 12:08:09 +0700

    script

--- app/model/wallet/migration/20200711030721-change-column-client-ip.js
+++ app/model/wallet/migration/20200711030721-change-column-client-ip.js
@@ -2,34 +2,26 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(t => {
-      return Promise.all([
-        queryInterface.changeColumn(
-          'user_ips',
-          'client_ip',
-          {
-            type: DataTypes.STRING(200),
-            allowNull: true
-          }
-        )
-      ]);
-    });
+    return queryInterface.changeColumn(
+      'user_ips',
+      'client_ip',
+      {
+        type: Sequelize.DataTypes.STRING(200),
+        allowNull: true
+      }
+    )
   },
 
   // eslint-disable-next-line no-unused-vars
   down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(t => {
-      return Promise.all([
-         queryInterface.changeColumn(
-          'user_ips',
-          'client_ip',
-          {
-            type: DataTypes.STRING(32),
-            allowNull: true
-          }
-        )
-      ]);
-    });
+    queryInterface.changeColumn(
+      'user_ips',
+      'client_ip',
+      {
+        type: Sequelize.DataTypes.STRING(32),
+        allowNull: true
+      }
+    )
   }
 };
 
