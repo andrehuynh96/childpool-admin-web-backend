ed8fe5bd73a3b5f7a6f25f5fdfefc078fea26560
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-07-13 14:32:34 +0700

    update data create and validator

--- app/feature/membership/bank-account/bank-account.request.schema.js
+++ app/feature/membership/bank-account/bank-account.request.schema.js
@@ -3,7 +3,6 @@ const Joi = require('joi');
 const schema = Joi.object().keys({
   bank_name: Joi.string().required(),
   branch_name: Joi.string().required(),
-  swift: Joi.string().required(),
   account_name: Joi.string().required(),
   account_number: Joi.string().required(),
   currency_symbol: Joi.string().required(),
