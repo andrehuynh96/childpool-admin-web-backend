67e140475ee295d467623a6f547a94e68ef9dcd3
nqt900 <nqt900@gmail.com>
2020-07-22 14:00:28 +0700

    change email model

--- app/model/wallet/email-template.js
+++ app/model/wallet/email-template.js
@@ -1,26 +1,21 @@
-const { Temporalize } = require('sequelize-temporalize');
 module.exports = (sequelize, DataTypes) => {
-  const Wallet = sequelize.define("email_templates", {
+  const Model = sequelize.define("email_templates", {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
       allowNull: false,
       defaultValue: DataTypes.UUIDV4(),
     },
-    organization: {
-      type: DataTypes.STRING(256),
-      allowNull: true
-    },
     name: {
       type: DataTypes.STRING(256),
       allowNull: true
     },
     subject:{
-      type: DataTypes.STRING(200),
+      type: DataTypes.STRING(1000),
       allowNull: true,
     },
     template: {
-      type: DataTypes.STRING(2000),
+      type: DataTypes.TEXT('long'),
       allowNull: true,
     },
     language: {
@@ -36,20 +31,6 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
     timestamps: true,
   });
-  Temporalize({
-    model: Wallet,
-    sequelize,
-    temporalizeOptions: {
-      blocking: false,
-      full: false,
-      modelSuffix: "_his"
-    }
-  });
-
-  Wallet.associate = (models) => {
-    Wallet.hasMany(models.wallet_tokens, { foreignKey: 'wallet_id', as: "tokens" });
-    Wallet.hasMany(models.wallet_priv_keys, { foreignKey: 'wallet_id', as: "privKeys" });
-    Wallet.belongsTo(models.members, { foreignKey: 'member_id', targetKey: 'id', as: "member" });
-  };
-  return Wallet;
+ 
+  return Model;
 }   
