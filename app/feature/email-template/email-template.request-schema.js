d75e8720a477f26510e1fa84dc887f2479cff342
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-07-23 15:57:56 +0700

    change api update email template

--- app/feature/email-template/email-template.request-schema.js
+++ app/feature/email-template/email-template.request-schema.js
@@ -1,8 +1,15 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-  subject: Joi.string().max(1000).required(),
-  template: Joi.string().required(),
+  email_templates: Joi.array().items(
+    Joi.object().keys({
+      name: Joi.string().max(256).required(),
+      subject: Joi.string().max(1000).required(),
+      template: Joi.string().required(),
+      language: Joi.string().max(20).required()
+    }
+    ).required()
+  ).required()
 });
 
 module.exports = schema;
