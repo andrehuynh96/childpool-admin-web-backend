c66fb59fef915d0e288351d5ff52d6b03f65a009
Unknown <huyht@blockchainlabs.asia>
2020-07-24 17:12:02 +0700

    allow coins
    allow coins

--- app/worker/job/update-affiliate.job.js
+++ app/worker/job/update-affiliate.job.js
@@ -9,7 +9,7 @@ const Op = Sequelize.Op;
 module.exports = {
   execute: async () => {
     try {
-      let coins = ["IRIS"];//, "ATOM", "ONT"
+      let coins = ["IRIS", "ATOM", "ONT"];//, "ATOM", "ONT"
       for (let element of coins) {
         // call account distribution
         let offset = 0;
