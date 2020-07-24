3a434b983433e08af5faacab3717b7f2624c2c87
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-07-24 11:09:09 +0700

    update validator

--- app/feature/membership/member-kyc/member-kyc.request-schema.js
+++ app/feature/membership/member-kyc/member-kyc.request-schema.js
@@ -4,7 +4,7 @@ const schema = Joi.object().keys({
   member_kyc_properties: Joi.array().items(
     Joi.object().keys({
       id: Joi.number().required(),
-      value: Joi.string().required()
+      value: Joi.string().allow('').optional()
     }
     ).required()
   ).required()
