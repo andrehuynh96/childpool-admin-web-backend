ab331e54f38e35cfed0f4580dad92279b2dda656
Unknown <huyht@blockchainlabs.asia>
2020-07-24 17:03:11 +0700

    fix affiliate_id
    fix affiliate_id

--- app/worker/job/update-affiliate.job.js
+++ app/worker/job/update-affiliate.job.js
@@ -9,7 +9,7 @@ const Op = Sequelize.Op;
 module.exports = {
   execute: async () => {
     try {
-      let coins = ["IRIS", "ATOM", "ONT"];//, "ATOM", "ONT"
+      let coins = ["IRIS"];//, "ATOM", "ONT"
       for (let element of coins) {
         // call account distribution
         let offset = 0;
@@ -74,7 +74,7 @@ module.exports = {
             let address = contribution.address;
             let amount = contribution.amount;
             let w = wallets.find(x => x.privKeys.filter(t => t.address == address).length > 0);
-            if (!w || !w.member || !w.member.affiliate_id <= 0) {
+            if (!w || !w.member || w.member.affiliate_id <= 0) {
               continue;
             }
             let email = w.member.email;
