beb00824992936d70c3328f30abe36249e4f30d6
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-07-24 14:31:54 +0700

    create each name

--- app/model/wallet/seed/email-template.js
+++ app/model/wallet/seed/email-template.js
@@ -1,42 +1,41 @@
 const EmailTemplate = require('app/model/wallet').email_templates;
 const EmailTemplateTypes = require('app/model/wallet/value-object/email-template-type');
 const fs = require('fs');
+const logger = require('app/lib/logger');
 const path = require("path");
-const config = require('app/config');
 
 module.exports = async () => {
     const emailNames = Object.values(EmailTemplateTypes);
-    const countEmailTemplates = await EmailTemplate.count({
-        where: {
-            name: emailNames
+    let root = path.resolve(
+        __dirname + "../../../../../public/email-template/moonstake/"
+    );
+    const data = [{
+        name: EmailTemplateTypes.MEMBERSHIP_ORDER_APPROVED,
+        subject: 'Membership payment',
+        template: fs.readFileSync(path.join(root, 'membership-order-approved.ejs'), 'utf-8'),
+    }, {
+        name: EmailTemplateTypes.MEMBERSHIP_ORDER_REJECTED,
+        subject: 'Membership payment',
+        template: fs.readFileSync(path.join(root, 'membership-order-rejected.ejs'), 'utf-8'),
+    }];
+
+    for (let item of emailNames) {
+        const emailTemplate = await EmailTemplate.findAll({
+            where: {
+                name: item,
+                language: ['en', 'jp']
+            }
+        });
+        if (emailTemplate.length === 0) {
+            const unavailableEmail = data.find(x => x.name === item);
+            const emailTemplateData = [{
+                ...unavailableEmail, language: 'en'
+            }, {
+                ...unavailableEmail, language: 'jp'
+            }];
+            
+            await EmailTemplate.bulkCreate(emailTemplateData,{ returning:true });
+            logger.info('insert email template');
         }
-    });
-    if (countEmailTemplates === 0) {
-        let root = path.resolve(
-            __dirname + "../../../../../public/email-template/"
-        );
-        const data = [{
-            name: EmailTemplateTypes.MEMBERSHIP_ORDER_APPROVED,
-            subject: 'Membership payment',
-            template: fs.readFileSync(`${root}/${config.emailTemplate.membershipOrderApproved}`, 'utf-8'),
-            language: 'en'
-        }, {
-            name: EmailTemplateTypes.MEMBERSHIP_ORDER_REJECTED,
-            subject: 'Membership payment',
-            template: fs.readFileSync(`${root}/${config.emailTemplate.membershipOrderRejected}`, 'utf-8'),
-            language: 'en'
-        },
-        {
-            name: EmailTemplateTypes.MEMBERSHIP_ORDER_APPROVED,
-            subject: 'Membership payment jp',
-            template: fs.readFileSync(`${root}/${config.emailTemplate.membershipOrderApproved}`, 'utf-8'),
-            language: 'jp'
-        }, {
-            name: EmailTemplateTypes.MEMBERSHIP_ORDER_REJECTED,
-            subject: 'Membership payment jp',
-            template: fs.readFileSync(`${root}/${config.emailTemplate.membershipOrderRejected}`, 'utf-8'),
-            language: 'jp'
-        }];
-        await EmailTemplate.bulkCreate(data, { returning: true });
     }
 };
