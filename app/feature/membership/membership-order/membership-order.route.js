38cfd5aa7c7aec7d4d770599755f74c3e48159af
My <myhn@blocjchainlabs.asia>
2020-07-31 14:04:35 +0700

    update swagger

--- app/feature/membership/membership-order/membership-order.route.js
+++ app/feature/membership/membership-order/membership-order.route.js
@@ -364,7 +364,8 @@ module.exports = router;
  *            - password
  *            example:
  *              {       
-                        "template": "MEMBERSHIP_ORDER_REJECTED_REASON_TIMED_OUT" || "note":"note"
+                        "template": "MEMBERSHIP_ORDER_REJECTED_REASON_TIMED_OUT",
+                         "note":"note"
                         
                 }
  *     produces:
