c1cc7f7e4779f6043248b832114b50a2acba9502
myhn <myhn@blockchainlabs.asia>
2020-08-11 14:35:25 +0700

    update swagger

--- app/feature/membership/crypto-address/crypto-address.route.js
+++ app/feature/membership/crypto-address/crypto-address.route.js
@@ -12,3 +12,89 @@ router.get('/crypto-address',
 );
 
 module.exports = router;
+
+/** *******************************************************************/
+/**
+ * @swagger
+ * /web/membership/crypto-address:
+ *   get:
+ *     summary: search crypto address
+ *     tags:
+ *       - Crypto address
+ *     description:
+ *     parameters:
+ *       - name: offset
+ *         in: query
+ *         type: integer
+ *         format: int32
+ *       - name: limit
+ *         in: query
+ *         type: integer
+ *       - name: platform
+ *         in: query
+ *         type: string
+ *       - name: address
+ *         in: query
+ *         type: string
+ *       - name: email
+ *         in: query
+ *         type: string
+ *       - name: membership_type_id
+ *         in: query
+ *         type: string
+ *       - name: reward
+ *         in: query
+ *         type: boolean
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         examples:
+ *           application/json:
+ *             {
+                    "data": {
+                        "items": [
+                            {
+                                "id": 1,
+                                "member_id": "8337b3e4-b8be-4594-bca3-d6dba7c751ea",
+                                "member_account_id": 1,
+                                "type": "Bank",
+                                "status": "Pending",
+                                "amount": "0",
+                                "currency_symbol": "ETH",
+                                "created_at": "2020-05-29T06:15:07.006Z"
+                            },
+                            {
+                                "id": 2,
+                                "member_id": "8337b3e4-b8be-4594-bca3-d6dba7c751ea",
+                                "member_account_id": 1,
+                                "type": "Bank",
+                                "status": "Approved",
+                                "amount": "0",
+                                "currency_symbol": "ETH",
+                                "created_at": "2020-05-29T06:15:07.006Z"
+                            }
+                        ],
+                        "offset": 0,
+                        "limit": 10,
+                        "total": 1
+                    }
+                }
+ *       400:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/400'
+ *       401:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       404:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/404'
+ *       500:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
