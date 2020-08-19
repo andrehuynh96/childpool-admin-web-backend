92802d71d77c421af963cd7c3ba5ba49152bc12b
THINHNQ <thinhnq@blockchainlabs.asia>
2020-08-19 11:50:28 +0700

    call service

--- app/worker/job/check-ada-pool-size.job.js
+++ app/worker/job/check-ada-pool-size.job.js
@@ -1,14 +1,14 @@
 const logger = require("app/lib/logger");
-const CheckTransactionDistributeReward = require("../service/check-distribute/index");
+const CheckAdaPoolSize = require("../service/check-ada-pool-size/index");
 
 module.exports = {
   execute: async () => {
     try {
-      let service = new CheckTransactionDistributeReward();
+      let service = new CheckAdaPoolSize();
       service.check();
     }
     catch (err) {
-      logger.error("check transaction distribute reward to member job error:", err);
+      logger.error("check ada pool size job error:", err);
     }
   }
 }
