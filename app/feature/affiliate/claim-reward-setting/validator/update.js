ee224de58736160563df8b27a6a8cb5cef3b5bd5
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2020-09-15 17:48:07 +0700

    Update min amount and network fee

--- app/feature/affiliate/claim-reward-setting/validator/update.js
+++ app/feature/affiliate/claim-reward-setting/validator/update.js
@@ -6,6 +6,12 @@ const schema = Joi.object().keys({
   claim_affiliate_reward_ong: Joi.number().min(0).optional(),
   claim_affiliate_reward_one: Joi.number().min(0).optional(),
   claim_affiliate_reward_xtz: Joi.number().min(0).optional(),
+
+  claim_affiliate_reward_atom_network_fee: Joi.number().min(0).optional(),
+  claim_affiliate_reward_iris_network_fee: Joi.number().min(0).optional(),
+  claim_affiliate_reward_ong_network_fee: Joi.number().min(0).optional(),
+  claim_affiliate_reward_one_network_fee: Joi.number().min(0).optional(),
+  claim_affiliate_reward_xtz_network_fee: Joi.number().min(0).optional(),
 });
 
 module.exports = schema;
