7f30daf5795a8ca06512721ec4a9d3dae911bf0b
quocltm <quocltm@blockchainlabs.asia>
2020-09-21 09:54:33 +0700

    Tracking daily reward HARMONY (fix return null)

--- app/worker/service/get-member-asset/one.js
+++ app/worker/service/get-member-asset/one.js
@@ -125,7 +125,7 @@ async function getAmountAndRewardONE(address, validatorAddresses) {
         };
     } catch (error) {
         logger.error(error);
-        throw error;
+        return null;
     }
 }
 
