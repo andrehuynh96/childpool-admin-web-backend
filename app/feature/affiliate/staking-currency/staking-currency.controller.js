64e5549fc616dfef6a15093c530f3e2ae576ec82
Hung Tran <hungtran.it13@gmail.com>
2020-09-23 14:31:26 +0700

    Fix: Get staking platforms

--- app/feature/affiliate/staking-currency/staking-currency.controller.js
+++ app/feature/affiliate/staking-currency/staking-currency.controller.js
@@ -2,22 +2,20 @@ const logger = require('app/lib/logger');
 const config = require("app/config");
 
 module.exports = {
-    getStakingCurrencyList: async (req, res, next) => {
-        try {
-          const stakingCurrencyStr = config.stakingCurrency;
-          const stakingCurrencies = stakingCurrencyStr.split(',');
-          const stakingCurrencyList = stakingCurrencies.map(item => {
-            return {
-              currency_symbol: item,
-              name: item,
-            };
-          });
-    
-          return res.ok(stakingCurrencyList);
-        }
-        catch (error) {
-          logger.error("get staking currency list fail", error);
-          next(error);
-        }
-      },
+  getStakingPlatforms: async (req, res, next) => {
+    try {
+      const stakingPlatforms = (config.stakingPlatform || '').split(',').map(item => {
+        return {
+          currency_symbol: item,
+          name: item,
+        };
+      });
+
+      return res.ok(stakingPlatforms);
+    }
+    catch (error) {
+      logger.error("get staking currency list fail", error);
+      next(error);
+    }
+  },
 };
