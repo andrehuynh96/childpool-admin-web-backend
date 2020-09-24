7f3f6f305b18df93164e905b1cedef879ac81cc1
Hung Tran <hungtv@blockchainlabs.asia>
2020-09-24 11:19:08 +0700

    Add network column

--- app/feature/currency/validator/update.js
+++ app/feature/currency/validator/update.js
@@ -11,6 +11,7 @@ const schema = Joi.object().keys({
   order_index: Joi.number().integer().allow(null).optional(),
   default_flg: Joi.boolean().required(),
   status: Joi.number().integer().required(),
+  network: Joi.string().max(100).required(),
   explore_url: Joi.string().max(500).allow('').allow(null).optional(),
   transaction_format_link: Joi.string().max(500).allow('').allow(null).optional(),
   address_format_link: Joi.string().max(500).allow('').allow(null).optional(),
