e6a758fea2666442cf2442e1c6dc9bae5fe6644f
Pham Ba Luan <luanpb@cardano-labo.com>
2020-09-24 10:47:32 +0700

    Update & send mail.

--- app/worker/job/sycn-currency-with-changelly.job.js
+++ app/worker/job/sycn-currency-with-changelly.job.js
@@ -1,11 +1,14 @@
 const exchangeCurrencies = require('../../model/wallet').exchange_currencies;
 const logger = require("app/lib/logger");
-const changelly = require('../../service/exchange/changelly');
+const Changelly = require('../../service/exchange/changelly');
+const SyncCurrencyChangellyServices = require("../service/Sync-currency-with-changelly");
+const config = require('app/config');
 
 module.exports = {
   execute: async () => {
     try {
-      const getCurrencies = new changelly().getCurrencies();
+      const getCurrencies = new Changelly().getCurrencies();
+      const syncCurrencyServices = new SyncCurrencyChangellyServices();
       const getChangellyData = await getCurrencies;
       const exchangeCurrenciesDB = await exchangeCurrencies.findAll();
       const changellyData = getChangellyData.result;
@@ -14,11 +17,12 @@ module.exports = {
 
         for (let i = 0; i < changellyData.length; i++) {
           const changellyTicker = changellyData[i].ticker;
+          const changellyPlatform = (changellyData[i].contract_address && changellyData[i].address_url.startsWith("https://etherscan.io")) ? "ETH" : changellyData[i].ticker.toUpperCase();
           const changellyEnabled = changellyData[i].enabled ? 1 : 0;
           const changellyFixRate = changellyData[i].fix_rate_enabled;
 
           exchangeCurrenciesDB.map(async (item) => {
-            if (changellyTicker.toLowerCase() === item.symbol.toLowerCase()) {
+            if (changellyTicker.toUpperCase() === item.symbol.toUpperCase() && changellyPlatform === item.platform.toUpperCase()) {
               if (changellyEnabled !== item.status || changellyFixRate !== item.fix_rate_flg) {
                 let update = {};
                 if (changellyEnabled !== item.status) update = { ...update, status: changellyEnabled };
@@ -28,6 +32,11 @@ module.exports = {
                     symbol: item.symbol
                   }
                 });
+                // sent email
+                if (config.sendMailToAdminFlg) {
+                  const data = { ...update, symbol: item.symbol, flatForm: changellyPlatform };
+                  syncCurrencyServices.sendMail(data);
+                }
                 // log
                 logger.info('Status/FixRate change: coin/token - ', item.symbol);
                 if (changellyEnabled !== item.status) logger.info('Status: ', "Changlelly: " + changellyEnabled + " - DB: " + item.status);
