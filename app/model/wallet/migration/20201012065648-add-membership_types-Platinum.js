00dc7fe6cb842b8da91473f90ca5abeff2fbf525
myhn <myhn@blockchainlabs.asia>
2020-10-12 17:39:26 +0700

    change query update membership

--- app/model/wallet/migration/20201012065648-add-membership_types-Platinum.js
+++ app/model/wallet/migration/20201012065648-add-membership_types-Platinum.js
@@ -7,15 +7,20 @@ module.exports = {
       return Promise.all([
         queryInterface.describeTable('membership_types')
           .then(async (tableDefinition) => {
-            const updatePriceSQL = `UPDATE membership_types SET (price,is_enabled) = (100,true) WHERE name= 'Platinum';
-            UPDATE membership_types Set (price,type) = (0,'Free') Where name= 'Gold'`;
-            await queryInterface.sequelize.query(updatePriceSQL, {}, {});
+            const updatePlatinumSQL = `UPDATE membership_types SET (price,is_enabled) = (100,true) WHERE name= 'Platinum'`;
+            await queryInterface.sequelize.query(updatePlatinumSQL, {}, {});
+
+            const updateGoldSQL =  `UPDATE membership_types Set price= 0 Where name= 'Gold'`;
+            await queryInterface.sequelize.query(updateGoldSQL, {}, {});
 
             const updateMembershipOrderSQL = `UPDATE membership_orders SET membership_type_id = (SELECT id FROM membership_types WHERE name = 'Platinum') WHERE status = 'Pending' `;
             await queryInterface.sequelize.query(updateMembershipOrderSQL, {}, {});
 
-            const updateMemberSQL = `UPDATE members SET membership_type_id = (SELECT id FROM membership_types WHERE name = 'Platinum') WHERE membership_type_id = (SELECT id FROM membership_types WHERE name = 'Gold');UPDATE members SET membership_type_id = (SELECT id FROM membership_types WHERE name = 'Gold') WHERE membership_type_id = (SELECT id FROM membership_types WHERE name = 'Silver') AND kyc_id = '3' AND kyc_level = 'LEVEL_2' AND kyc_status = 'Approved' AND referrer_code IS NOT NULL`;
-            await queryInterface.sequelize.query(updateMemberSQL, {}, {});
+            const updateGoldMemberSQL = `UPDATE members SET membership_type_id = (SELECT id FROM membership_types WHERE name = 'Platinum') WHERE membership_type_id = (SELECT id FROM membership_types WHERE name = 'Gold')`;
+            await queryInterface.sequelize.query(updateGoldMemberSQL, {}, {});
+
+            const updateSilverMemberSQL = `UPDATE members SET membership_type_id = (SELECT id FROM membership_types WHERE name = 'Gold') WHERE membership_type_id = (SELECT id FROM membership_types WHERE name = 'Silver') AND kyc_id = '3' AND kyc_level = 'LEVEL_2' AND kyc_status = 'Approved' AND referrer_code IS NOT NULL`;
+            await queryInterface.sequelize.query(updateSilverMemberSQL, {}, {});
 
             return Promise.resolve();
           })
