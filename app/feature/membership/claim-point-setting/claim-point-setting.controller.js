8ec01cd9130d2dc93bf0616700a3dcf6602fa15c
myhn <myhn@blockchainlabs.asia>
2020-10-19 15:44:44 +0700

    api get update ms points config

--- app/feature/membership/claim-point-setting/claim-point-setting.controller.js
+++ app/feature/membership/claim-point-setting/claim-point-setting.controller.js
@@ -1,19 +1,73 @@
 const logger = require('app/lib/logger');
 const Setting = require('app/model/wallet').settings;
+const MembershipType = require('app/model/wallet').membership_types;
+const database = require('app/lib/database').db().wallet;
 module.exports = {
   get: async (req, res, next) => {
     try {
-      const settings = await Setting.findAll();
-      return res.ok(settings);
-    } catch (error) {
+      const settings = await Setting.findOne({
+        where: {
+          key: 'MS_POINT_DELAY_TIME_IN_SECONDS'
+        }
+      });
+      const delayTime = settings.value;
+      const membershipTypes = await MembershipType.findAll({
+        attributes: ['name','claim_points'],
+        where: {
+          is_enabled: true
+        },
+        raw: true
+      });
+
+      const claimPoints = membershipTypes.reduce((result,value) => {
+        result[value.name] = value.claim_points;
+        return result;
+      },{});
+      const results = {
+        claimPoints: claimPoints,
+        delayTime: delayTime
+      };
+
+      return res.ok(results);
+    }
+    catch (error) {
       logger.info('get ms point settings fail',error);
       next();
     }
   },
   update: async (req, res, next) => {
+    let transaction;
     try {
+      const { delayTime, claimPoints } = req.body;
+      const transaction = await database.transaction();
+      await Setting.update({
+        value: delayTime
+      }, {
+        where: {
+          key: 'MS_POINT_DELAY_TIME_IN_SECONDS'
+        },
+        returning: true,
+        transaction: transaction
+      });
+      for (let item in claimPoints) {
+        await MembershipType.update({
+          claim_points: claimPoints[item]
+        },{
+          where: {
+            name: item
+          },
+          returning: true,
+          transaction: transaction
+        });
+      }
+      await transaction.commit();
+
       return res.ok(true);
-    } catch (error) {
+    }
+    catch (error) {
+      if (transaction) {
+        transaction.rollback();
+      }
       logger.info('update ms point settings fail',error);
       next();
     }
--- app/feature/membership/claim-point-setting/validator/update.js
+++ app/feature/membership/claim-point-setting/validator/update.js
@@ -3,7 +3,11 @@ const { Schemas } = require("aws-sdk");
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-  claimPoints: Joi.number().min(1).required(),
-  delayTimes: Joi.number().min(1).required()
+  claimPoints: Joi.object().keys({
+    Silver: Joi.number().min(1).required(),
+    Gold: Joi.number().min(1).required(),
+    Platinum: Joi.number().min(1).required(),
+  }).required(),
+  delayTime: Joi.number().min(1).required()
 });
 module.exports = schema;
