95b11ad7c5e612277e5f1af9373c8da2b40372f5
myhn <myhn@blockchainlabs.asia>
2020-10-20 14:33:19 +0700

    min 0 claim point and delay time

--- app/feature/membership/claim-point-setting/validator/update.js
+++ app/feature/membership/claim-point-setting/validator/update.js
@@ -4,10 +4,10 @@ const Joi = require('joi');
 
 const schema = Joi.object().keys({
   claimPoints: Joi.object().keys({
-    Silver: Joi.number().min(1).required(),
-    Gold: Joi.number().min(1).required(),
-    Platinum: Joi.number().min(1).required(),
+    Silver: Joi.number().min(0).required(),
+    Gold: Joi.number().min(0).required(),
+    Platinum: Joi.number().min(0).required(),
   }).required(),
-  delayTime: Joi.number().min(1).required()
+  delayTime: Joi.number().min(0).required()
 });
 module.exports = schema;
