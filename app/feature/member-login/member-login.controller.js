f78d24ed082f11fffa53afd51b768d540b021a29
myhn <myhn@blockchainlabs.asia>
2020-10-29 17:30:49 +0700

    update api tracking logins

--- app/feature/member-login/member-login.controller.js
+++ app/feature/member-login/member-login.controller.js
@@ -8,27 +8,119 @@ const Op = Sequelize.Op;
 
 
 module.exports = {
-  get: async (req,res,next) => {
+  get: async (req, res, next) => {
     try {
       const endMonth = moment.utc().endOf('day');
-      const startMonth = moment(endMonth).subtract(30,'day');
-      const where = {
-        created_at: {
-          [Op.gte]: startMonth,
-          [Op.lt]: endMonth
+      const startMonth = moment(endMonth).subtract(29,'day').startOf('day');
+      const startWeek = moment(endMonth).subtract(6,'day').startOf('day');
+      const startDay = moment(endMonth).subtract(24,'hour');
+
+      const loginsMonth = await MemberActivityLog.findAll({
+        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('member_id')), 'member_id']],
+        where: {
+          created_at: {
+            [Op.gte]: startMonth,
+            [Op.lt]: endMonth
+          },
+          action: ActionType.LOGIN
         },
-        action: ActionType.LOGIN
+      });
+
+      const loginsWeek = await MemberActivityLog.findAll({
+        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('member_id')), 'member_id']],
+        where: {
+          created_at: {
+            [Op.gte]: startWeek,
+            [Op.lt]: endMonth
+          },
+          action: ActionType.LOGIN
+        },
+      });
+
+      const loginsDay = await MemberActivityLog.findAll({
+        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('member_id')), 'member_id']],
+        where: {
+          created_at: {
+            [Op.gte]: startDay,
+            [Op.lt]: endMonth
+          },
+          action: ActionType.LOGIN
+        },
+      });
+
+      const totalMonth = loginsMonth.length;
+      const totalWeek = loginsWeek.length;
+      const totalDay = loginsDay.length;
+
+      const allMemberLoginMonth = await MemberActivityLog.count({
+        where: {
+          created_at: {
+            [Op.gte]: startMonth,
+            [Op.lt]: endMonth
+          },
+          action: ActionType.LOGIN
+        },
+        group: ['created_at']
+      });
+
+      allMemberLoginMonth.forEach(item => {
+        item.created_at = moment(item.created_at).format('YYYY-MM-DD');
+      });
+
+      const dayOfMonth = allMemberLoginMonth.map(item => item.created_at);
+      const uniqDayOfMonth = _.uniq(dayOfMonth).sort();
+
+      const resultOfMonth = countLoginEachTimes(allMemberLoginMonth,uniqDayOfMonth);
+
+      const month = {
+        totalMonth: totalMonth,
+        items: resultOfMonth
       };
 
-      const memberLogins = await MemberActivityLog.findAll({
-        attributes: ['member_id','created_at'],
-        where: where,
-        order: [['created_at','DESC']],
-        raw: true
+      const allMemberLoginWeek = [];
+      const dayOfStartWeek = moment(startWeek).format('YYYY-MM-DD');
+      const dayOfWeek = [];
+      uniqDayOfMonth.forEach(item => {
+        if (item >= dayOfStartWeek) {
+          dayOfWeek.push(item);
+        }
       });
-      const memberLoginMonth = countDataMonthTo30Day(memberLogins,startMonth,endMonth);
 
-      return res.ok(memberLoginMonth);
+      const resultOfWeek = countLoginEachTimes(allMemberLoginMonth,dayOfWeek);
+      const week = {
+        totalWeek: totalWeek,
+        resultOfWeek: resultOfWeek
+      };
+
+      const allMemberLoginDay = await MemberActivityLog.count({
+        where: {
+          created_at: {
+            [Op.gte]: startDay,
+            [Op.lt]: endMonth
+          },
+          action: ActionType.LOGIN
+        },
+        group: ['created_at']
+      });
+
+      allMemberLoginDay.forEach(item => {
+        item.created_at = moment(item.created_at).format('YYYY-MM-DD HH:00');
+      });
+      const hourOfDay = allMemberLoginDay.map(item => item.created_at);
+      const uniqHourOfDay = _.uniq(hourOfDay).sort();
+
+      const resultOfDay = countLoginEachTimes(allMemberLoginDay,uniqHourOfDay);
+
+      const day = {
+        totalDay: totalDay,
+        items: resultOfDay
+      };
+
+      return res.ok({
+        month: month,
+        week: week,
+        day: day
+      });
     }
     catch (error) {
       logger.error('Get member login chart fail',error);
@@ -36,22 +128,15 @@ module.exports = {
   }
 };
 
-function countDataDayTo24Hour() {
-  return [];
-}
-
-function countMonthAndWeek(memberLoggins,startMonth,endMonth) {
-  const members = memberLoggins.map(item => item.member_id);
-  const totalMonth = _.uniq(members);
-  // const result = [];
-
-  const cache = memberLoggins.reduce((result,value) => {
-    const day = moment(value.created_at).format('YYYY-MM-DD');
-    result[day] = value.member_id;
-  },[]);
-  console.log(cache);
-
-  return {
-    totalMonth: totalMonth
-  };
+function countLoginEachTimes(allLogin,times) {
+  const result = {};
+  times.forEach(item => {
+    result[item] = 0;
+    allLogin.forEach(x => {
+      if (x.created_at === item) {
+        result[item] += parseInt(x.count);
+      }
+    });
+  });
+  return result;
 }
