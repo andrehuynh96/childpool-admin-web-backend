3279d1a9c0276bab42cda783facb1c9ec22d365f
myhn <myhn@blockchainlabs.asia>
2020-10-30 09:47:12 +0700

    remove chart data

--- app/feature/member-login/member-login.controller.js
+++ app/feature/member-login/member-login.controller.js
@@ -1,6 +1,5 @@
 const logger = require('app/lib/logger');
 const moment = require('moment');
-const _ = require('lodash');
 const MemberActivityLog = require('app/model/wallet').member_activity_logs;
 const ActionType = require("app/model/wallet/value-object/member-activity-action-type");
 const Sequelize = require('sequelize');
@@ -52,70 +51,12 @@ module.exports = {
       const totalWeek = loginsWeek.length;
       const totalDay = loginsDay.length;
 
-      const allMemberLoginMonth = await MemberActivityLog.findAll({
-        attributes: [
-          [Sequelize.fn('date_trunc', 'day', Sequelize.col('created_at')), 'day'],
-          [Sequelize.literal(`COUNT('member_id')`), 'count']],
-        where: {
-          created_at: {
-            [Op.gte]: startMonth,
-            [Op.lt]: endMonth
-          },
-          action: ActionType.LOGIN
-        },
-        group: ['day'],
-        raw: true
-      });
-
-      const month = {
-        totalMonth: totalMonth,
-        items: allMemberLoginMonth
-      };
-
-      const allMemberLoginWeek = await MemberActivityLog.findAll({
-        attributes: [
-          [Sequelize.fn('date_trunc', 'day', Sequelize.col('created_at')), 'day'],
-          [Sequelize.literal(`COUNT('member_id')`), 'count']],
-        where: {
-          created_at: {
-            [Op.gte]: startWeek,
-            [Op.lt]: endMonth
-          },
-          action: ActionType.LOGIN
-        },
-        group: ['day'],
-        raw: true
-      });
-
-      const week = {
-        totalMonth: totalMonth,
-        items: allMemberLoginWeek
-      };
-
-      const allMemberLoginDay = await MemberActivityLog.findAll({
-        attributes: [
-          [Sequelize.fn('date_trunc', 'hour', Sequelize.col('created_at')), 'hour'],
-          [Sequelize.literal(`COUNT('member_id')`), 'count']],
-        where: {
-          created_at: {
-            [Op.gte]: startDay,
-            [Op.lt]: endMonth
-          },
-          action: ActionType.LOGIN
-        },
-        group: ['hour'],
-        raw: true
-      });
-      const day = {
-        totalDay: totalDay,
-        items: allMemberLoginDay
-      };
 
 
       return res.ok({
-        month: month,
-        week: week,
-        day: day
+        month: totalMonth,
+        week: totalWeek,
+        day: totalDay
       });
     }
     catch (error) {
