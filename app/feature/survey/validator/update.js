fa7db87f49ff364e35067112f12fb0ad4e9722e5
myhn <myhn@blockchainlabs.asia>
2020-11-18 11:53:20 +0700

    update survey, question and answer

--- app/feature/survey/validator/update.js
+++ app/feature/survey/validator/update.js
@@ -2,14 +2,14 @@ const Joi = require('joi');
 const QuestionType = require("app/model/wallet/value-object/question-type");
 
 const answerSchema = Joi.object().keys({
-  id: Joi.number().required(),
+  id: Joi.number().optional(),
   text: Joi.string().max(2000).required(),
   text_ja: Joi.string().allow(null).allow('').max(2000).optional(),
   is_correct_flg: Joi.boolean().required(),
 });
 
 const questionSchema = Joi.object().keys({
-  id: Joi.number().required(),
+  id: Joi.number().optional(),
   title: Joi.string().required(),
   title_ja: Joi.string().allow(null).allow('').optional(),
   question_type: Joi.string().valid(Object.values(QuestionType)).required(),
