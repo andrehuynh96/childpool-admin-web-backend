2e271757948e39e3cfd8d30849bf440508be0b68
myhn <myhn@blockchainlabs.asia>
2020-12-23 13:55:35 +0700

    remove console.log

--- app/lib/logger/log4js.js
+++ app/lib/logger/log4js.js
@@ -1,7 +1,6 @@
 const config = require('app/config');
 const { createLogger } = require('staking-logging');
 
-console.log(config.app.name);
 const logger = createLogger(config.app.name);
 
 module.exports = logger;
