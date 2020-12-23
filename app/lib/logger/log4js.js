f4f90fa57653f8edee6d834c6765a3d668f974a9
myhn <myhn@blockchainlabs.asia>
2020-12-23 13:54:33 +0700

    update version staking-logging

--- app/lib/logger/log4js.js
+++ app/lib/logger/log4js.js
@@ -1,6 +1,7 @@
 const config = require('app/config');
 const { createLogger } = require('staking-logging');
 
+console.log(config.app.name);
 const logger = createLogger(config.app.name);
 
 module.exports = logger;
