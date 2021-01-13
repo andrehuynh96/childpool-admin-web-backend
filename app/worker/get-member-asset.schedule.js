9a8276a95e05754cefb6a067e7572a8f2d2cf95a
huyht-ibl <huyht@blockchainlabs.asia>
2021-01-13 10:56:38 +0700

    get member asset

--- app/worker/get-member-asset.schedule.js
+++ app/worker/get-member-asset.schedule.js
@@ -5,11 +5,12 @@ const runWithLockFile = require('app/lib/run-lock-file');
 const GET_MEMBER_ASSET_LOCK_FILE = 'get_member_asset.lock';
 
 module.exports = {
-  run: async () => {
-    ///cron.schedule(config.schedule.getMemberAsset, async () => {
-    //  await runWithLockFile(getMemberAsset, GET_MEMBER_ASSET_LOCK_FILE, "get member asset", true);
-    //});
-    await getMemberAsset.execute();
+  run: () => {
+    cron.schedule(config.schedule.getMemberAsset, async () => {
+      // await runWithLockFile(getMemberAsset, GET_MEMBER_ASSET_LOCK_FILE, "get member asset", true);
+      await getMemberAsset.execute();
+    });
+
   },
   lockFile: () => GET_MEMBER_ASSET_LOCK_FILE
 };
